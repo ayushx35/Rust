d3841e57f17adf040a447f28a86b5814f6561069
Ayushmaan Singh <ayushmaansingh0305@gmail.com>
2026-05-08 11:51:03 +0530

    add

--- if_else/src/main.rs
+++ if_else/src/main.rs
@@ -1,11 +1,13 @@
 fn main() {
-    println!("{}", is_even(2));
+    println!("{}", is_even(0));
 }
 
-fn is_even(num: i32) -> bool {
-    if num % 2 == 0 {
-        return true;
+fn is_even(num: i32) -> String {
+    if num == 0 {
+        return "Really !!!... creative".to_string();
+    } else if num % 2 == 0 {
+        return "true".to_string();
     } else {
-        return false;
+        return "false".to_string();
     }
 }
