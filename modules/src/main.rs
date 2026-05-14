f978a845359169fcafc2a83e66b514de82e0fcd0
Ayushmaan Singh <ayushmaansingh0305@gmail.com>
2026-05-15 00:14:33 +0530

    add module

---
+++ modules/src/main.rs
@@ -0,0 +1,30 @@
+use hello1::hello1 as hq;
+use hello1::hi1 as tt;
+fn main() {
+    hq();
+    tt::hiq();
+    hello::hello();
+    hello::hi::hi();
+}
+
+mod hello {
+    pub fn hello() {
+        println!("Hello!");
+    }
+    pub mod hi {
+        pub fn hi() {
+            println!("hi");
+        }
+    }
+}
+
+mod hello1 {
+    pub fn hello1() {
+        println!("Hello! there");
+    }
+    pub mod hi1 {
+        pub fn hiq() {
+            println!("hi there");
+        }
+    }
+}
